f1988a3b3b125f17e157fbda9f572b98e938c913
Jakub Kubacki <kubacki.jk@gmail.com>
2015-11-05 23:32:53 +0100

    Add search form

--- app/controllers/tvs_controller.rb
+++ app/controllers/tvs_controller.rb
@@ -1,6 +1,16 @@
 class TvsController < ApplicationController
   def index
-    redirect_to :back, alert: 'No name provided' unless params.key?(:name) && params[:name].present?
-    @results = TvSearch.new(name: params[:name]).call
+    redirect_to :back, alert: 'No name provided' unless valid_params?
+    @results = TvSearch.new(name: search_param).call
+  end
+
+  private
+
+  def valid_params?
+    params.key?(:search) && search_param.present?
+  end
+
+  def search_param
+    params[:search][:name]
   end
 end
