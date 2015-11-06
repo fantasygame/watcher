fe5f53c7eaaf8ec5a8babd38a938b70e701cd380
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-11-06 19:35:57 +0100

    Display TVs image in tvs index via helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def logo_image_path(image_path, size = 3, alt_img)
+    return unless image_path.present?
+    configuration = Tmdb::Configuration.new
+    "<img src='#{configuration.base_url + configuration.logo_sizes[size] + image_path}' alt='#{alt_img}'>"
+  end
 end
