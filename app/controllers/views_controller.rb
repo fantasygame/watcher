c0ec8f42f83df91c3349c8c3ad6a96c8ed2657cd
Jakub Kubacki <kubacki.jk@gmail.com>
2015-11-07 15:28:39 +0100

    See unseen all episodes in season

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -10,16 +10,22 @@ class ViewsController < ApplicationController
   end
 
   def set_all
-    SetAsSeen.new(current_user, Tv.find(params[:tv_id])).call
+    SetAsSeen.new(current_user, Tv.find(params[:tv_id]), season_episodes).call
     redirect_to :back
   end
 
   def unset_all
-    SetAsUnseen.new(current_user, Tv.find(params[:tv_id])).call
+    SetAsUnseen.new(current_user, Tv.find(params[:tv_id]), season_episodes).call
     redirect_to :back
   end
 
   private
+
+  def season_episodes
+    season = nil
+    season = Season.find(params[:tv_id], params[:season_id]) if params[:season_id].present?
+    season.present? ? season.episodes : []
+  end
 
   def view_params
     params.require(:view).permit(:tv_id, :episode_id)
--- app/services/set_as_unseen.rb
+++ app/services/set_as_unseen.rb
@@ -1,6 +1,4 @@
 class SetAsUnseen
-  attr_reader :user, :tv, :episodes
-
   def initialize(user, tv, episodes = [])
     @user = user
     @tv = tv
@@ -8,10 +6,10 @@ class SetAsUnseen
   end
 
   def call
-    episodes = tv.episodes if @episodes.empty?
-    episodes.each do |episode|
-      if user.seen?(episode)
-        View.where(tv_id: @tv.id, episode_id: episode.id, user: user).destroy_all
+    @episodes = @tv.episodes if @episodes.empty?
+    @episodes.each do |episode|
+      if @user.seen?(episode)
+        View.where(tv_id: @tv.id, episode_id: episode.id, user: @user).destroy_all
       end
     end
   end
