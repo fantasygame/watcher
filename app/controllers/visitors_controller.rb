9520f479464c3cea0e3f28351fbcc76becc9d148
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-11-08 20:16:50 +0100

    Render episodes to see as home page for logged in users

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,8 @@
 class VisitorsController < ApplicationController
+  def index
+    if user_signed_in?
+      @results = EpisodesToSee.new(current_user).call
+      render 'episodes/to_see'
+    end
+  end
 end
