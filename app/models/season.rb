4f5a37acb98c198a91257364bce9937ab279dc3d
Jakub Kubacki <kubacki.jk@gmail.com>
2015-11-08 16:50:00 +0100

    Sort episodes in season desc by default

--- app/models/season.rb
+++ app/models/season.rb
@@ -5,8 +5,10 @@ class Season < Resource
     build Tmdb::Season.detail(tv_id, number)
   end
 
-  def episodes
-    episodes_summary.map { |episode_summary| Episode.build(episode_summary) }
+  def episodes(sort: :desc)
+    episodes = episodes_summary.map { |episode_summary| Episode.build(episode_summary) }
+    episodes.reverse! if sort == :desc
+    episodes
   end
 
   def episodes=(data)
