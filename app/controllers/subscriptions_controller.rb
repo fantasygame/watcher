1692ed3c7520d352dcc59d3edad9275607659156
Jakub Kubacki <kubacki.jk@gmail.com>
2016-03-02 20:31:44 +0100

    Set S01E01 as default

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -4,7 +4,12 @@ class SubscriptionsController < ApplicationController
     if current_user.subscriptions.find_by(resource_id: params[:resource_id]).present?
       redirect_to :back, alert: 'Already in Your resources.'
     else
-      Subscription.find_or_create_by(user_id: current_user.id, resource_id: params[:resource_id])
+      Subscription.find_or_create_by(
+        user_id: current_user.id,
+        resource_id: params[:resource_id],
+        episode: 1,
+        season: 1
+      )
       redirect_to :back, notice: 'Resource has been added'
     end
   end
