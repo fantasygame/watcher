b11287997cb861652ca3e30338a5f5741f27630b
Jakub Kubacki <kubacki.jk@gmail.com>
2016-04-09 22:04:09 +0200

    Add episode marker for tvname s00e00 format

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,8 @@
 class Episode < Resource
+  def episode_marker(tv_name)
+    "#{tv_name} #{season_marker}"
+  end
+
   def season_marker
     "S#{season_number.to_s.rjust(2, '0')}E#{episode_number.to_s.rjust(2, '0')}"
   end
