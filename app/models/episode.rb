a8481bec6b149b3f083ece82cf37cde849514fdd
Jakub Kubacki <kubacki.jk@gmail.com>
2016-04-09 22:04:25 +0200

    Add torrent links

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -6,4 +6,21 @@ class Episode < Resource
   def season_marker
     "S#{season_number.to_s.rjust(2, '0')}E#{episode_number.to_s.rjust(2, '0')}"
   end
+
+  def torrent_link(tv_name, type)
+    "#{base_torrent_link(type)}/#{URI::encode(episode_marker(tv_name))}"
+  end
+
+  private
+
+  def base_torrent_link(type)
+    case type
+    when :kickass
+      "https://kat.cr/usearch"
+    when :thepiratebay
+      "https://thepiratebay.se/search"
+    else
+      fail "Unknown type"
+    end
+  end
 end
