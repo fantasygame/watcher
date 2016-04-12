badba53b319b008336f74bbd9864b481690ec058
Jakub Kubacki <kubacki.jk@gmail.com>
2016-04-12 21:35:44 +0200

    More rubocop issues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
-  def logo_image_path(image_path, size = 3, alt_img)
+  def logo_image_path(image_path, size, alt_img)
     if image_path.present?
       configuration = Tmdb::Configuration.new
-      "<img src='#{configuration.base_url + configuration.logo_sizes[size] + image_path}' alt='#{alt_img}'>"
+      src = configuration.base_url + configuration.logo_sizes[size] + image_path
+      "<img src='#{src}' alt='#{alt_img}'>"
     else
       no_image(size, alt_img)
     end
