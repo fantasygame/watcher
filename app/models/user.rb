6731f11435455ae51297a10e751bb884ded528b6
Jakub Kubacki <kubacki.jk@gmail.com>
2016-09-01 21:27:20 +0200

    Add devise to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :subscriptions
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, if: :new_record?
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  devise_for :users
+  root to: "visitors#index"
   resources :tvs, only: [:index, :show] do
     resources :seasons, only: [:show]
     resources :episodes, only: [:index]
